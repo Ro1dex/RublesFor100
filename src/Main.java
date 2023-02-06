61c94d415c19eab916ba033322389bf38ca2ea88
Ro1dex <sanhcez58@gmail.com>
2023-02-06 22:56:26 +0200

    start

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        int cash = 100;
+        int addCash = 100;
+        int bonus;
+        if (addCash > 1000) {
+            bonus = addCash / 100;
+        }
+        else {
+            bonus = 0;
+        }
+        int balance = cash + addCash;
 
-        System.out.println();
+        System.out.println("Вас счет составляет: " + balance + " рублей." + " Бонусных рублей начислено: " + bonus);
     }
 }
